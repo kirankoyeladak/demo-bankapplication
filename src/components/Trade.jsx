5aa524cf1d23a43d69b9c27d306e7a01b94f3e64
eeswarbuddha <72460734+eeswarbuddha@users.noreply.github.com>
2020-10-08 22:29:10 -0700

    Trade Section alignments done
    Trade Section alignments done

--- src/components/Trade.jsx
+++ src/components/Trade.jsx
@@ -54,61 +54,64 @@ export default function Trade(){
             <h6><i className="fas fa-arrow-circle-down mr-2 mt-1 ml-2"></i> Currency Exchange </h6>
         </div>
         <div id="trade" class="container bg-white br-10 mt-3 p-4 shadow-sm">
-           
-            <div>
-                <label>Amount</label>
-                <Textbox name='amount' type='number' value={amount} handleChange={handleChange}/>
-            </div>
-            <div class="d-flex mt-4">
-                <div class="flex-fill mr-2">
-                    <label>From</label>
-                    <select value={fromTrade} onChange={(e)=>{
-                        setFromTrade(e.target.value)
-                    }} class="custom-select">
-                        <option selected>Select</option>
-                        <option value='1'>INR</option>
-                        <option value='19'>MYR</option>
-                        <option value='70'>USD</option>
-                    </select>
+            
+                <div class="row">
+                    <div class="col-4 d-flex flex-column">
+                        <label>Amount</label>
+                        <Textbox name='amount' type='number' value={amount} handleChange={handleChange}/>
+                    </div>
+                    <div class="col-4">
+                    
+                        <label>From</label>
+                        <select value={fromTrade} onChange={(e)=>{
+                            setFromTrade(e.target.value)
+                        }} class="custom-select">
+                            <option selected>Select</option>
+                            <option value='1'>INR</option>
+                            <option value='19'>MYR</option>
+                            <option value='70'>USD</option>
+                        </select>
+                    </div>
+                    <div class="col-4">
+                        <label>To</label>
+                        <select value={toTrade} onChange={(e)=>{
+                            if(e.target.value != "select"){
+                                setToTrade(e.target.value);
+                                calculateTrade(e.target.value);
+                            }
+                            else
+                            setToTrade(0);
+                        }} class="custom-select">
+                            <option value='select' selected>Select</option>
+                            <option value='1'>INR</option>
+                            <option value='19'>MYR</option>
+                            <option value='70'>USD</option>
+                        </select>
+                    </div>
                 </div>
-                <div class="flex-fill">
-                    <label>To</label>
-                    <select value={toTrade} onChange={(e)=>{
-                        if(e.target.value != "select"){
-                            setToTrade(e.target.value);
-                            calculateTrade(e.target.value);
-                        }
-                        else
-                        setToTrade(0);
-                    }} class="custom-select">
-                        <option value='select' selected>Select</option>
-                        <option value='1'>INR</option>
-                        <option value='19'>MYR</option>
-                        <option value='70'>USD</option>
-                    </select>
-                </div>
-            </div>
-            <div class="my-4 d-flex">
-                <div class="flex-fill mr-2">
-                    <label>Trade Money</label>
-                    <div class="bg-warning p-2">{toTrade}</div>
-                </div>
-                <div class="flex-fill mr-2">
-                    <label>Calculate Amount</label>
-                    <div class="bg-warning p-2">{tradeAmount}</div>
-                </div>
-                {/* <div class="flex-fill">
-                    <label>Amount</label>
-                    <Textbox name='convertTradeMoney' type='number' value={tradeAmount} handleChange={handleChange}/> 
-                </div> */}
-                <div class="flex-fill">
-                    <label>Current Trade Amount</label>
-                    <Textbox name='convertTradeMoney' type='number' value={tradeAmount} handleChange={handleChange}/> 
-                </div>
-                <div class="align-self-end ml-2">
-                    <button class='btn btn-info ml-auto br-10' onClick={handleSubmit}>Trade</button>
+                <div id="trade-wrapper" class="row mt-3 bg-light">
+                    <div class="col-md-4 py-2 py-md-3 align-self-end">
+                        <label>Trade Money</label>
+                        <div class="trade-money">|{toTrade}</div>
+                    </div>
+                    <div class="col-md-4 py-md-3 align-self-end">
+                        <label>Calculate Amount</label>
+                        <div class="trade-money">{tradeAmount}</div>
+                    </div>
+                    {/* <div class="flex-fill">
+                        <label>Amount</label>
+                        <Textbox name='convertTradeMoney' type='number' value={tradeAmount} handleChange={handleChange}/> 
+                    </div> */}
+                    <div class="col-md-4 mt-2 mt-md-3">
+                        <label>Current Trade Amount</label>
+                        <Textbox name='convertTradeMoney' type='number' value={tradeAmount} handleChange={handleChange}/> 
+                    </div>
                 </div>    
-            </div>
+                    <div class="mt-3 text-right">
+                        <button class='btn btn-info ml-auto br-10' onClick={handleSubmit}>Trade</button>
+                    </div>    
+                
+              
             <ToastContainer />
         </div>
         </>
